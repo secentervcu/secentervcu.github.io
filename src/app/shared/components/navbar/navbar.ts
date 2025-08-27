cdb07c519c8ad701d904a1d1e46a3d92c02f866a
Victória Gomes <victoria.oliveiragomes@gmail.com>
2025-08-27 12:48:11 -0400

    feat: fixed part of the scroll problem

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {TranslatePipe} from '@ngx-translate/core';
 import {RouterLink} from '@angular/router';
+import {ViewportScroller} from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -13,4 +14,17 @@ import {RouterLink} from '@angular/router';
   ]
 })
 export class Navbar {
+  @ViewChild('nav') nav!: ElementRef<HTMLElement>;
+
+  constructor(private viewportScroller: ViewportScroller) {
+  }
+
+  public scrollToSection(sectionId: string): void {
+    this.viewportScroller.setOffset([0, this.getNavHeight()]);
+    this.viewportScroller.scrollToAnchor(sectionId);
+  }
+
+  private getNavHeight(): number {
+    return this.nav.nativeElement.offsetHeight;
+  }
 }
