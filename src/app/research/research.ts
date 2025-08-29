aa9890110776357040d1f32718d89024ab808b87
Victória Gomes <victoria.oliveiragomes@gmail.com>
2025-08-29 19:29:30 -0400

    feat: added research section

--- src/app/research/research.ts
+++ src/app/research/research.ts
@@ -3,6 +3,7 @@ import {LogoDisplay} from '../shared/components/logo-display/logo-display';
 import {Navbar} from '../shared/components/navbar/navbar';
 import {About} from './components/about/about';
 import {Team} from './components/team/team';
+import {LabResearch} from './components/lab-research/lab-research';
 
 @Component({
   selector: 'app-research',
@@ -10,7 +11,8 @@ import {Team} from './components/team/team';
     Navbar,
     LogoDisplay,
     About,
-    Team
+    Team,
+    LabResearch
   ],
   templateUrl: './research.html',
   styleUrl: './research.sass'
