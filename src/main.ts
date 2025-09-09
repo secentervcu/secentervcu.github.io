5c710d5ea87b2f9ba1850e739dc269bdb537b947
Victória Gomes <victoria.oliveiragomes@gmail.com>
2025-09-09 11:31:02 -0400

    feat: removed all files

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { register } from 'swiper/element/bundle';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
-
-register();
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
