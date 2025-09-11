3eeeb15e38187b0d51a850b63a2100a8176a36f6
Bruno Claudino Matias <38812683+brunoclaudino@users.noreply.github.com>
2025-09-11 15:59:43 -0400

    Add website info and HTML compatibility
    Added informations from the doc file. Also added safeHtml to website accept html inside the json content file.
    Bug correction on collaboration swipper

--- src/app/research/components/about/about.ts
+++ src/app/research/components/about/about.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {TranslatePipe} from '@ngx-translate/core';
+import {SafeHtmlPipe} from '../../../shared/pipes/safe-html.pipe';
 
 @Component({
   selector: 'app-about',
   imports: [
-    TranslatePipe
+    TranslatePipe,
+    SafeHtmlPipe
   ],
   templateUrl: './about.html',
   styleUrl: './about.sass'
