85c3e55a8e655a03f4a749c45bc8cfd2f8f0f5a2
Victória Gomes <victoria.oliveiragomes@gmail.com>
2025-09-13 13:00:44 -0400

    feat: updated colors and removed colorpicker

--- src/app/research/components/lab-research/lab-research.ts
+++ src/app/research/components/lab-research/lab-research.ts
@@ -1,57 +1,55 @@
 import { Component } from '@angular/core';
 import {TranslatePipe} from '@ngx-translate/core';
-import {ColorPicker} from '../../../shared/components/color-picker/color-picker';
 
 @Component({
   selector: 'app-lab-research',
   imports: [
-    TranslatePipe,
-    ColorPicker
+    TranslatePipe
   ],
   templateUrl: './lab-research.html',
   styleUrl: './lab-research.sass'
 })
 export class LabResearch {
-  selectedBackgroundColor = {
-    'name': 'Pantone 130C and 109U 60%',
-    'value': 'rgba(255, 179, 0, 0.6)'
+  selectedBackgroundColor =     {
+    'name': 'Pantone 130C and 109U 80%',
+    'value': 'rgba(255, 179, 0, 0.8)'
   };
   selectedTextColor = {
     'name': 'Black',
     'value': '#000000'
   };
-  selectedCardColor = {
-    'name': 'Blue',
-    'value': '#006894'
-  };
   selectedCardTextColor = {
     'name': 'Black',
     'value': '#000000'
   };
-  selectedCardHoverColor = {
+  selectedCardColor =     {
     'name': 'Grey',
     'value': '#DEDEDE'
   };
+  selectedCardHoverColor = {
+    'name': 'White',
+    'value': 'rgb(255, 255, 255)'
+  };
 
-  changeBackgroundColor(color: any) {
-    this.selectedBackgroundColor = color;
-  }
-
-  changeTextColor(color: any) {
-    this.selectedTextColor = color;
-  }
-
-  changeCardTextColor(color: any) {
-    this.selectedCardTextColor = color;
-  }
-
-  changeCardColor(color: any) {
-    this.selectedCardColor = color;
-  }
-
-  changeCardHoverColor(color: any) {
-    this.selectedCardHoverColor = color;
-  }
+  // changeBackgroundColor(color: any) {
+  //   this.selectedBackgroundColor = color;
+  // }
+  //
+  // changeTextColor(color: any) {
+  //   this.selectedTextColor = color;
+  // }
+  //
+  // changeCardTextColor(color: any) {
+  //   this.selectedCardTextColor = color;
+  // }
+  //
+  // changeCardColor(color: any) {
+  //   this.selectedCardColor = color;
+  // }
+  //
+  // changeCardHoverColor(color: any) {
+  //   this.selectedCardHoverColor = color;
+  // }
 
   onHover(event: MouseEvent, hoverColor: any) {
     const target = event.target as HTMLElement;
