f91f7db5415e15b7678b4416dd7987ebe759d70b
Victória Gomes <victoria.oliveiragomes@gmail.com>
2025-10-04 09:43:29 -0400

    feat: added team photos and information

--- src/app/research/components/team/team.ts
+++ src/app/research/components/team/team.ts
@@ -1,12 +1,21 @@
 import {Component} from '@angular/core';
 import {TranslatePipe} from '@ngx-translate/core';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {faLinkedin, faXTwitter} from '@fortawesome/free-brands-svg-icons';
+import {faEnvelope, faGlobe} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-team',
   imports: [
-    TranslatePipe
+    TranslatePipe,
+    FaIconComponent
   ],
   templateUrl: './team.html',
   styleUrl: './team.sass'
 })
-export class Team  {}
+export class Team  {
+  protected readonly faLinkedin = faLinkedin;
+  protected readonly faEnvelope = faEnvelope;
+  protected readonly faGlobe = faGlobe;
+  protected readonly faXTwitter = faXTwitter;
+}
